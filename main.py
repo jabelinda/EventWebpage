a17ff6ce1c779cc338b9954df06da81dcc0453e4
Anton <103533816+MorgonNyheter@users.noreply.github.com>
2023-03-01 11:05:28 +0100

    Add register form with images and css.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -12,5 +12,15 @@ def index():
     return render_template('index.html', title="Home")
 
 
+@app.route("/register")
+def register():
+    return render_template('register.html', title="Sign in")
+
+
+@app.route("/postevent")
+def postevent():
+    return render_template('postevent.html', title="Post Event")
+
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=7000, debug=True)
+    app.run(host="0.0.0.0", port=7001, debug=True)
